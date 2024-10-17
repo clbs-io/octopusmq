a2f0053d92cc1d8b7feac89d5e50d6a5489f7b1a
Jan Klaban <jan.klaban@ddash.cz>
2024-10-17 18:20:02 +0200

    all in one stream

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -37,50 +38,6 @@ func NewClient(target string, grpcOptions ...grpc.DialOption) *Client {
 		grpcClient: grpcClient,
 		mgmtClient: mgmtClient,
 	}
-}
-
-func (c *Client) Push(ctx context.Context, req *pb.EnqueueRequest, opts ...grpc.CallOption) (*pb.EnqueueResponse, error) {
-	return c.svcClient.Push(ctx, req, opts...)
-}
-
-func (c *Client) Prepend(ctx context.Context, req *pb.EnqueueRequest, opts ...grpc.CallOption) (*pb.EnqueueResponse, error) {
-	return c.svcClient.Prepend(ctx, req, opts...)
-}
-
-func (c *Client) Pull(ctx context.Context, req *pb.PullRequest, opts ...grpc.CallOption) (*pb.PullResponse, error) {
-	return c.svcClient.Pull(ctx, req, opts...)
-}
-
-func (c *Client) PullSingle(ctx context.Context, req *pb.PullSingleRequest, opts ...grpc.CallOption) (*pb.PullSingleResponse, error) {
-	return c.svcClient.PullSingle(ctx, req, opts...)
-}
-
-func (c *Client) CommitSingle(ctx context.Context, req *pb.CommitSingleRequest, opts ...grpc.CallOption) (*pb.CommitSingleResponse, error) {
-	return c.svcClient.CommitSingle(ctx, req, opts...)
-}
-
-func (c *Client) Commit(ctx context.Context, req *pb.CommitRequest, opts ...grpc.CallOption) (*pb.CommitResponse, error) {
-	return c.svcClient.Commit(ctx, req, opts...)
-}
-
-func (c *Client) RequeueSingle(ctx context.Context, req *pb.RequeueSingleRequest, opts ...grpc.CallOption) (*pb.RequeueSingleResponse, error) {
-	return c.svcClient.RequeueSingle(ctx, req, opts...)
-}
-
-func (c *Client) Requeue(ctx context.Context, req *pb.RequeueRequest, opts ...grpc.CallOption) (*pb.RequeueResponse, error) {
-	return c.svcClient.Requeue(ctx, req, opts...)
-}
-
-func (c *Client) DeleteSingle(ctx context.Context, req *pb.DeleteSingleRequest, opts ...grpc.CallOption) (*pb.DeleteSingleResponse, error) {
-	return c.svcClient.DeleteSingle(ctx, req, opts...)
-}
-
-func (c *Client) Delete(ctx context.Context, req *pb.DeleteRequest, opts ...grpc.CallOption) (*pb.DeleteResponse, error) {
-	return c.svcClient.Delete(ctx, req, opts...)
-}
-
-func (c *Client) Subscribe(ctx context.Context, opts ...grpc.CallOption) (*Subscriber, error) {
-	panic("not implemented")
 }
 
 func (c *Client) Close() error {
