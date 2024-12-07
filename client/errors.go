0fbe204880efc51f5ac645cf766ebbd9993e73e7
Jan Klaban <jan.klaban@ddash.cz>
2024-12-07 17:17:24 +0100

    storage errors

--- client/errors.go
+++ client/errors.go
@@ -11,4 +11,10 @@ var (
 	ErrQueueClientClosed  = errors.New("queue client closed")
 	ErrQueueClientEof     = errors.New("queue client EOF")
 	ErrQueueTimeout       = errors.New("queue operation timeout")
+
+	ErrStorageNotFound      = errors.New("storage not found")
+	ErrStorageAlreadyExists = errors.New("storage already exists")
+	ErrStorageClientClosed  = errors.New("storage client closed")
+	ErrStorageClientEof     = errors.New("storage client EOF")
+	ErrStorageTimeout       = errors.New("storage operation timeout")
 )
